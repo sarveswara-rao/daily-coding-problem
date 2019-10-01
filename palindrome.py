5773ca25084425bca9e25c67f9973a80f6055a68
sarveswara-rao <sarveswararao.cs@yahoo.com>
2019-10-01 16:21:18 +0530

    palindrome programme

---
+++ palindrome.py
@@ -0,0 +1,29 @@
+
+# accept a string from user
+s = input('Enter a string: ')
+#====================================
+# METHOD 1
+print('Using Method 1: ', end = ' ')
+# to flag when we come across non matching character
+flag = 0
+
+# compare from beg.. and from end
+for i in range(len(s)//2):
+    if s[i] != s[-(i+1)]:
+        flag = 1
+        break
+# check the flag
+if flag == 1:
+    print(f'"{s}" is not a palindrome')
+else:
+    print(f'"{s}" is a palindrome')
+#======================================================
+
+# Method two
+print('Using Method 2: ', end = ' ')
+rev_s = ''.join(list(reversed(s)))
+
+if s == rev_s:
+    print(f'"{s}" is a palindrome')
+else:
+    print(f'"{s}" is not a palindrome')
