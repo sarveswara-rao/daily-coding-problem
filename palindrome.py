141e6288d8a4414e03d631a5440c280a64f3eee6
sarveswara-rao <sarveswararao.cs@yahoo.com>
2019-10-01 16:46:53 +0530

    added some commnet

--- palindrome.py
+++ palindrome.py
@@ -1,24 +1,25 @@
 
 # accept a string from user
 s = input('Enter a string: ')
-#====================================
-# METHOD 1
+#======================================================#
+# Method 1
 print('Using Method 1: ', end = ' ')
 # to flag when we come across non matching character
 flag = 0
-
 # compare from beg.. and from end
 for i in range(len(s)//2):
+    # if two characters are not equal set the flag
     if s[i] != s[-(i+1)]:
         flag = 1
+        # if True then the break from the loop
         break
 # check the flag
 if flag == 1:
     print(f'"{s}" is not a palindrome')
 else:
     print(f'"{s}" is a palindrome')
-#======================================================
-
+    
+#======================================================#
 # Method two
 print('Using Method 2: ', end = ' ')
 rev_s = ''.join(list(reversed(s)))
@@ -27,3 +28,4 @@ if s == rev_s:
     print(f'"{s}" is a palindrome')
 else:
     print(f'"{s}" is not a palindrome')
+#======================================================#
