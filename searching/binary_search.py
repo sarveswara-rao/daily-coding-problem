2e3561361d89c364dbc755391774e98bb9bb3cc2
sarveswara-rao <sarveswararao.cs@yahoo.com>
2019-10-10 17:11:49 +0530

    4

---
+++ searching/binary_search.py
@@ -0,0 +1,27 @@
+a = [1, 2, 6, 7, 12, 15, 19, 40, 90, 100, 123]
+
+# Binary search Iterative Version
+
+print(f"Given Array: {a}")
+n = int(input('Enter the element to search: '))
+
+flag = False
+# lower, higher, and middle index
+l = 0
+h = len(a) - 1
+m = (l+h)//2
+
+while l < h:
+    if a[m] == n:
+        flag = True
+        break
+    elif n > a[m]:
+        l = m + 1
+    else:
+        h = m - 1
+    m = (l+h)//2
+
+if flag:
+    print('Element is found')
+else:
+    print('Element is not found')
